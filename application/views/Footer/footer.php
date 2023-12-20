6d2a2ae9b43fc05d29c13c1c9a80a911653a8bc7
RaymonLeo <137139476+RaymonLeo@users.noreply.github.com>
2023-12-20 07:51:32 +0700

    Create footer.php

---
+++ application/views/Footer/footer.php
@@ -0,0 +1,23 @@
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<script src="<?= base_url('assets') ?>/js/libs/jquery-3.1.1.min.js"></script>
+<script src="<?= base_url('bootstrap') ?>/js/popper.min.js"></script>
+<script src="<?= base_url('bootstrap') ?>/js/bootstrap.min.js"></script>
+<script src="plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="<?= base_url('assets') ?>/js/app.js"></script>
+
+<script>
+    $(document).ready(function() {
+        App.init();
+    });
+</script>
+<script src="plugins/highlight/highlight.pack.js"></script>
+<script src="<?= base_url('assets') ?>/js/custom.js"></script>
+<!-- END GLOBAL MANDATORY STYLES -->
+
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="<?= base_url('assets') ?>/js/scrollspyNav.js"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+
+</body>
+
+</html>
