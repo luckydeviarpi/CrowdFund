0cf77f361734178a740332759cea19a885158426
RaymonLeo <137139476+RaymonLeo@users.noreply.github.com>
2024-01-03 06:17:31 +0700

    Portofolio_selesai

--- application/controllers/Portofolio.php
+++ application/controllers/Portofolio.php
@@ -29,6 +29,22 @@ class Portofolio extends CI_Controller
         $this->load->view("Footer/footer", $data);
     }
 
+    public function portofolioselesai()
+    {
+        $data['judul'] = "Halaman Portofolio";
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        // Modify this line to include the total_investasi for each usaha
+        $data['usaha'] = $this->getUsahaWithTotalInvestasi();
+        $user_id = $data['user']['id'];
+        $data['user_money'] = $this->Usaha_model->getTotalModal($user_id);
+
+        $this->load->view("Header/header", $data);
+        $this->load->view("Portofolio/vw_portofolio_selesai", $data);
+        $this->load->view("Footer/footer", $data);
+    }
+
+
     // Add this new function to get usaha with total_investasi
     private function getUsahaWithTotalInvestasi()
     {
