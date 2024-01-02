366a92d396d8953e11cb70f5a2d8f4297c1248e1
luckydeviarpi <lucky22ti@mahasiswa.pcr.ac.id>
2024-01-02 19:26:00 +0700

    Memperbaiki link resources

--- application/views/Footer/footer.php
+++ application/views/Footer/footer.php
@@ -2,19 +2,17 @@
 <script src="<?= base_url('assets') ?>/js/libs/jquery-3.1.1.min.js"></script>
 <script src="<?= base_url('bootstrap') ?>/js/popper.min.js"></script>
 <script src="<?= base_url('bootstrap') ?>/js/bootstrap.min.js"></script>
-<script src="<?= base_url('plugins')?>/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="<?= base_url('plugins') ?>/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="<?= base_url('assets') ?>/js/app.js"></script>
-
+<script src="<?= base_url('plugins') ?>/highlight/highlight.pack.js"></script>
+<script src="<?= base_url('assets') ?>/js/custom.js"></script>
+<script src="<?= base_url('assets') ?>/js/elements/custom-search.js"></script>
+<script src="<?= base_url('assets') ?>/js/scrollspyNav.js"></script>
 <script>
     $(document).ready(function() {
         App.init();
     });
 </script>
-
-<script src="<?= base_url('plugins')?>/highlight/highlight.pack.js"></script>
-<script src="<?= base_url('plugins') ?>/highlight/highlight.pack.js"></script>
-
-<script src="<?= base_url('assets') ?>/js/custom.js"></script>
 <!-- END GLOBAL MANDATORY STYLES -->
 
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
