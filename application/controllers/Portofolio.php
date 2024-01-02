bf27ff28201efbedec7337be0204e6f2e13f3f11
RaymonLeo <137139476+RaymonLeo@users.noreply.github.com>
2024-01-03 05:45:36 +0700

    a

--- application/controllers/Portofolio.php
+++ application/controllers/Portofolio.php
@@ -7,9 +7,11 @@ class Portofolio extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('Usaha_model', 'usaha'); // Load the model with an alias 'usaha'
+        $this->load->model('User_model', 'userrole'); // Assuming you have a Portofolio_model
         $this->load->model('Usaha_model');
         $this->load->model('Portofolio_model'); // Assuming you have a Portofolio_model
-        $this->load->model('User_model');
+
     }
 
     public function index()
@@ -17,32 +19,37 @@ class Portofolio extends CI_Controller
         $data['judul'] = "Halaman Portofolio";
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-        // Fetch the user's money value
+        // Modify this line to include the total_investasi for each usaha
+        $data['usaha'] = $this->getUsahaWithTotalInvestasi();
         $user_id = $data['user']['id'];
         $data['user_money'] = $this->Usaha_model->getTotalModal($user_id);
 
-        // Fetch data usaha
-        $data['usaha'] = $this->Usaha_model->getByUserId($user_id);
-
         $this->load->view("Header/header", $data);
-        $this->load->view("portofolio/vw_portofolio", $data);
+        $this->load->view("Portofolio/vw_portofolio", $data);
         $this->load->view("Footer/footer", $data);
     }
 
-    public function portofolioselesai()
+    // Add this new function to get usaha with total_investasi
+    private function getUsahaWithTotalInvestasi()
     {
-        $data['judul'] = "Halaman Portofolio";
+        $usahaList = $this->usaha->get();
+
+        foreach ($usahaList as &$us) {
+            $us['total_investasi'] = $this->usaha->getInvestasiSum($us['id_usaha']);
+
+            // Get the count of records in the investasi table for the current usaha
+            $us['jumlah_investor'] = $this->usaha->getInvestasiCount($us['id_usaha']);
+        }
+
+        return $usahaList;
+    }
+
+    public function detail($id)
+    {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-
-        // Fetch the user's money value
-        $user_id = $data['user']['id'];
-        $data['user_money'] = $this->Portofolio_model->get_user_money($user_id);
-
-        // Fetch user's completed portofolio data
-        $data['usaha_selesai'] = $this->Usaha_model->getByUserId($user_id, 'Selesai');
-
+        $data['usaha_detail'] = $this->usaha->getById($id); // Use the loaded model 'usaha' instead of 'Usaha_model'
         $this->load->view("Header/header", $data);
-        $this->load->view("portofolio/vw_portofolio_selesai", $data);
-        $this->load->view("Footer/footer", $data);
+        $this->load->view("Investasi/vw_detailusaha", $data);
+        $this->load->view("Footer/footer");
     }
 }
--- application/views/portofolio/vw_portofolio.php
+++ application/views/portofolio/vw_portofolio.php
@@ -47,7 +47,7 @@ $active_page = 'berjalan'; // Atur halaman aktif, misalnya 'berjalan' atau 'sele
         <div class="layout-px-spacing">
             <h1 class="h3 mb-4 text-gray-800"><?php echo $judul; ?></h1>
             <!-- Add the following lines after the h1 tag -->
-    
+
             <div class="card">
                 <div class="card-body">
 
@@ -60,36 +60,48 @@ $active_page = 'berjalan'; // Atur halaman aktif, misalnya 'berjalan' atau 'sele
 
                 </div>
             </div>
-            
+
             <div class="row layout-spacing">
-                <!-- Content -->
-                <div class="row layout-top-spacing">
-                    <a href="<?= base_url('Test/detail') ?>" class="card-link">
-                        <div class="card component-card_9" style="width: 100%; display: flex; flex-direction: row; max-height: fit-content; overflow: hidden;">
-                            <div class="col-md-6">
-                                <img src="<?= base_url('assets') ?>/img/momoyo.png" class="card-img-top" style="width: 100%; height: 100%; object-fit: cover;" alt="widget-card-2">
-                            </div>
-                            <div class="col-md-6">
-                                <div class="card-body">
-                                    <h5 class="card-title">Ini Title Usaha</h5>
-                                    <p class="card-text">Ini Deskripsi singkat Usahanya.</p>
-                                    <div class="meta-info" style="font-size: 18px;">
-                                        <div class="row">
-                                            <div class="col"><b>Jumlah Dana</b></div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col"><b>Rp 6.600.000</b></div>
+                <?php foreach ($usaha as $us) : ?>
+                    <div class="col-xl-6 layout-top-spacing">
+                        <a href="<?= base_url('Investasi/detail/') . $us['id_usaha']; ?>" class="card-link">
+                            <div class="card">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="<?= base_url('assets/img/' . $us['foto1']); ?>" class="img-fluid rounded-start" alt="<?= $us['nama_usaha']; ?>">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title"><?= $us['nama_usaha']; ?></h5>
+                                            <p class="card-text"><?= $us['deskripsi']; ?></p>
+                                            <small class=" p-o-percentage mr-6">Terkumpul <b><?= number_format($us['total_investasi'], 0, ',', '.'); ?></b> dari <b><?= number_format($us['modal_akhir'], 0, ',', '.'); ?></b></small>
+                                            <?php
+                                            $progress = ($us['modal_akhir'] != 0) ? number_format(($us['total_investasi'] / $us['modal_akhir']) * 100, 2) : '0.00';
+                                            ?>
+                                            <small class="p-o-percentage"><?= $progress; ?>%</small>
+                                            <div class="progress br-30 mt-2">
+                                                <div class="progress-bar bg-success" role="progressbar" style="width: <?= $progress; ?>%" aria-valuenow="<?= $progress; ?>" aria-valuemin="0" aria-valuemax="100"></div>
+                                            </div>
+                                            <div class="meta-info mt-2">
+                                                <div class="row">
+                                                    <div class="col">Jumlah Investor</div>
+                                                    <div class="col">Status</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col"><b><?= $us['jumlah_investor']; ?></b> Investor</div>
+                                                    <div class="col"><b><?= $us['status']; ?></b></div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
-    </div>
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
